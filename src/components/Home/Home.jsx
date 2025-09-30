fa49ba28bf9eb7cd9a05a1c91cc77e35a1f3319f
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-30 09:28:02 +0600

    banner partially done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import { useLoaderData } from 'react-router-dom';
+import ServiceCard from '../ServiceCard/ServiceCard';
 
 const Home = () => {
+
+    const services = useLoaderData();
+
     return (
-        <div>
-            Home
+        <div className='text-red-500 text-2xl'>
+            <Banner></Banner>
+            <div>
+                {
+                    services.map(service => <ServiceCard></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
